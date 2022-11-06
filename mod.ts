b12305cdc4168248d708ecb06408ec16926f600e
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-11-07 02:02:22 +0600

    correct import_map

--- mod.ts
+++ mod.ts
@@ -1,20 +1,7 @@
-import { serve } from "deno:http";
 import { webhookCallback } from "grammy";
-
+import { Application } from "oak";
 import bot from "./bot.ts";
 
-const handleUpdate = webhookCallback(bot, "std/http");
+const app = new Application();
 
-serve(async (req) => {
-  if (req.method === "POST") {
-    const url = new URL(req.url);
-    if (url.pathname.slice(1) === bot.token) {
-      try {
-        return await handleUpdate(req);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
-  return new Response();
-});
+app.use(webhookCallback(bot, "oak"));
