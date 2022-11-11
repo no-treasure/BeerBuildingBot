4521017e82e26cf721af8c7495a388c3e8c8ac8f
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-11-11 17:36:38 +0600

    add bot.api.setWebhook in production mode

--- mod.ts
+++ mod.ts
@@ -4,12 +4,25 @@ import { serve } from "deno:http";
 import bot from "./bot.ts";
 
 const ENVIRONMENT = Deno.env.get("ENV");
+const BOT_URL = Deno.env.get("BOT_URL");
 
-if (ENVIRONMENT === "dev") {
-  bot.start();
-} else {
+const start = () => {
+  if (ENVIRONMENT === "dev") {
+    bot.start();
+
+    return;
+  }
+
+  if (!BOT_URL) {
+    throw new Error("BOT_URL env not provided");
+  }
+
+  bot.api.setWebhook(BOT_URL);
+
   serve(webhookCallback(bot, "std/http"));
-}
+};
+
+start();
 
 console.info(
   `Bot started in ${ENVIRONMENT === "dev" ? "development" : "production"} mode`,
