c00ecbb5b3dc55e605643f429db8cd4e09d919a0
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-11-11 15:14:30 +0600

    refactor bot.ts add env/load.ts

--- bot.ts
+++ bot.ts
@@ -1,16 +1,15 @@
-import { config } from "deno:dotenv";
+import "deno:dotenv/load";
+
 import { Bot, session } from "grammy";
 import { freeStorage } from "grammy/storage";
 
-import { AppContext, Command, SessionStorage } from "./domain/index.ts";
+import { AppContext, SessionStorage } from "./domain/index.ts";
 import AppModule from "./modules/index.ts";
 import { initialStorage } from "./utils/initial-storage.ts";
 
-const env = await config();
+const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
 
-const BOT_TOKEN = Deno.env.get("BOT_TOKEN") || env.BOT_TOKEN;
-
-if (!BOT_TOKEN) throw new Error("Missing BOT_TOKEN");
+if (!BOT_TOKEN) throw new Error("Missing BOT_TOKEN env");
 
 const bot = new Bot<AppContext>(BOT_TOKEN);
 
@@ -21,17 +20,8 @@ bot.use(
   }),
 );
 
-bot.command(Command.HELLO, (ctx) => {
-  ctx.reply("Greeting, Beer lovers!");
-});
-
-bot.catch((err) => {
-  console.error(
-    `Error while handling update ${err.ctx.update.update_id}:`,
-  );
-  console.error(err.error);
-});
-
 bot.use(AppModule);
+
+bot.catch((err) => console.error(err.error));
 
 export default bot;
