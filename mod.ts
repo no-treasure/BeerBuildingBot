fb927db8edb7ad42555c205969f67559c324a5dc
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2022-11-11 17:54:05 +0600

    cleanup entry

--- mod.ts
+++ mod.ts
@@ -3,27 +3,17 @@ import { serve } from "deno:http";
 
 import bot from "./bot.ts";
 
-const ENVIRONMENT = Deno.env.get("ENV");
 const BOT_URL = Deno.env.get("BOT_URL");
+const IS_PROD = Deno.env.get("MODE") === "production";
 
-const start = () => {
-  if (ENVIRONMENT === "dev") {
-    bot.start();
-
-    return;
-  }
-
-  if (!BOT_URL) {
-    throw new Error("BOT_URL env not provided");
-  }
-
+if (IS_PROD && BOT_URL) {
   bot.api.setWebhook(BOT_URL);
 
   serve(webhookCallback(bot, "std/http"));
-};
-
-start();
+} else {
+  bot.start();
+}
 
 console.info(
-  `Bot started in ${ENVIRONMENT === "dev" ? "development" : "production"} mode`,
+  `Bot started in ${IS_PROD ? "production" : "development"} mode`,
 );
